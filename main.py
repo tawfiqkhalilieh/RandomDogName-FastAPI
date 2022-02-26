5deb8222e24ca7f33f1531494b7dc8f1dfebc540
tawfiq khalilieh <87164562+xtawfeeq7k@users.noreply.github.com>
2022-02-26 09:06:49 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import json
+import random
+from fastapi import *
+
+@app.get("/dogs")
+def dogs():
+    with open('dogs.json', 'r') as f:
+        rnd = random.randrange(706)
+        data = json.load(f)
+        return data["names"][rnd]
+     
+     
+if __name__=="__main__":
+    uvicorn.run("main:app",host='0.0.0.0', port=4557, reload=True, debug=True, workers=3)
